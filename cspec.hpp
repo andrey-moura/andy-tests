ffb6c96992ee16f77e35adda51d8499a96fa06bc
andrey moura <andreymds88@gmail.com>
2022-07-24 16:19:22 -0300

    Starting to come to something

--- cspec.hpp
+++ cspec.hpp
@@ -2,6 +2,8 @@
 #include <iostream>
 #include <vector>
 #include <functional>
+#include <format>
+#include <sstream>
 
 #define CONCATENATE_DIRECT(s1, s2) s1##s2
 #define CONCATENATE(s1, s2) CONCATENATE_DIRECT(s1, s2)
@@ -21,21 +23,85 @@ namespace uva
     namespace cspec
     {
         class test_group;
+
+        class test_not_passed : public std::runtime_error
+        {
+        public:
+            test_not_passed(const std::string& what)
+                : std::runtime_error(what.c_str())
+            {
+
+            }
+        };
+
+        template<class T>
+        class eq
+        {
+        public:
+            eq(const T& t)
+                : m_expected(&t)
+            {
+
+            }
+        public:
+            const T* m_expected = nullptr;
+        };
+
+        template<typename T>
+        class expect
+        {
+        public:
+            expect(const T& t)
+                : m_expected(&t)
+            {
+
+            }
+        public:
+            const T* m_expected = nullptr;
+            bool m_expect_result = true;
+        public:
+            const expect<T>& to() const
+            {
+                expect<T>* this_non_const = (expect<T>*)this;
+                this_non_const->m_expect_result = true;
+                return *this;
+            }
+            const expect<T>& to_not() const
+            {
+                expect<T>* this_non_const = (expect<T>*)this;
+                this_non_const->m_expect_result = true;
+                return *this;
+            }
+            template<typename OtherT>
+            friend const expect<T>& operator<<(const expect<T>& expect, const eq<OtherT>& matcher)
+            {
+                bool match = *expect.m_expected == *matcher.m_expected;
+                bool passed = match ? expect.m_expect_result : !expect.m_expect_result;
+
+                if(!passed) {
+                    throw test_not_passed(std::format("Expected {}\ngot      {}", *expect.m_expected, *matcher.m_expected));
+                }
+
+                return expect;
+            }
+        };
+
         class core
         {
         public:
-            static std::vector<test_group*> s_tests_groups;
+            static std::vector<test_group*>& get_groups();
         public:
             static void run_tests();
         };
+
         class test
         {
         public:
             std::string m_name;
-            std::function<bool()> m_body;
+            std::function<void()> m_body;
         public:
             test() = default;
-            test(const std::string& name, const std::function<bool()> body)
+            test(const std::string& name, const std::function<void()> body)
                 : m_name(name), m_body(body)
             {
 
@@ -47,11 +113,7 @@ namespace uva
             std::string m_name;
             std::vector<test*> m_tests;
         public:
-            test_group(const std::string& name, const std::vector<test*>& tests) 
-                : m_name(name), m_tests(tests)
-            {
-                uva::cspec::core::s_tests_groups.push_back(this);
-            }
+            test_group(const std::string& name, const std::vector<test*>& tests);
         };
     };
 };
--- src/cspec.cpp
+++ src/cspec.cpp
@@ -1,25 +1,74 @@
-#include <cspec.hpp>
+#include <sstream>
 
-std::vector<uva::cspec::test_group*> uva::cspec::core::s_tests_groups;
+#include <cspec.hpp>
+#include <console.hpp>
+#include <diagnostics.hpp>
+
+//CORE BEGIN
+
+std::vector<uva::cspec::test_group*>& uva::cspec::core::get_groups()
+{
+    static std::vector<uva::cspec::test_group*> s_tests_groups;
+    return s_tests_groups;
+}
 
 void uva::cspec::core::run_tests()
 {
-    for(const auto& test_group : s_tests_groups)
-    {
-        for(const auto& test : test_group->m_tests)
+    std::vector<std::string> failed_messages;
+    std::vector<uva::cspec::test_group*> groups = uva::cspec::core::get_groups();
+
+    size_t examples = 0;
+    size_t failures = 0;
+
+    auto elapsed = uva::diagnostics::measure_function([&]() {
+        for(const auto& test_group : groups)
         {
-            try {
-                bool passed = test->m_body();
+            std::cout << test_group->m_name << std::endl;
 
-                if(passed) {
+            for(const auto& test : test_group->m_tests)
+            {
+                examples++;
 
-                } else {
+                try {
+                    test->m_body();
+                    std::cout << "\t" << uva::console::color(uva::console::color_code::green) << test->m_name << std::endl;
+                } catch(uva::cspec::test_not_passed& e)
+                {
+                    failures++;
+                    std::cout << "\t" << uva::console::color(uva::console::color_code::red) << test->m_name << std::endl;
 
+                    failed_messages.push_back(std::format("{} {}\nTest resulted in following error:\n{}", test_group->m_name, test->m_name, e.what()));
                 }
-                
-            } catch(std::exception& e) {
+                catch(std::exception& e)
+                {
+                    failures++;
+                    std::cout << "\t" << uva::console::color(uva::console::color_code::red) << test->m_name << std::endl;
 
+                    failed_messages.push_back(std::format("{} {}\nTest thrown following exception:\n{}", test_group->m_name, test->m_name, e.what()));
+                }
             }
+
+            std::cout << std::endl;
         }
+    });
+
+    for(const std::string& message : failed_messages)
+    {
+        std::cout << uva::console::color(uva::console::color_code::red) << message << std::endl;
     }
+
+    std::cout << std::endl;
+    std::cout << failures << " failures / " << examples << " examples." << std::endl;
+    std::cout << "Tests took " << elapsed.count() << "s" << std::endl;
+}
+
+//CORE END
+
+//TEST GROUP BEGIN
+
+uva::cspec::test_group::test_group(const std::string& name, const std::vector<uva::cspec::test*>& tests) 
+    : m_name(name), m_tests(tests)
+{
+    std::vector<uva::cspec::test_group*>& groups = uva::cspec::core::get_groups();
+    groups.push_back(this);
 }
