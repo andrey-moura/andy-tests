c07c852e275a3cf7d21ca7274a9c832b4531523a
andrey moura <andreymds88@gmail.com>
2022-08-20 17:54:13 -0300

    Starting to have something

--- cspec.hpp
+++ cspec.hpp
@@ -3,25 +3,34 @@
 #include <vector>
 #include <functional>
 #include <format>
+#include <filesystem>
 #include <sstream>
+
+#include <string.hpp>
 
 #define CONCATENATE_DIRECT(s1, s2) s1##s2
 #define CONCATENATE(s1, s2) CONCATENATE_DIRECT(s1, s2)
 
-#define cpsec_configure() \
+#define cspec_configure() \
     int main() {\
+        uva::cspec::temp_folder = CSPEC_TEMP_FOLDER;\
         uva::cspec::core::run_tests();\
         return 0;\
     }
 
-
-#define cspec_describe(group_title, examples) uva::cspec::test_group* CONCATENATE(test_line_, __LINE__) =  new uva::cspec::test_group(group_title, { examples })
+#define cspec_describe(group_title, examples) uva::cspec::test_group* CONCATENATE(test_line_, __LINE__) =  new uva::cspec::test_group(group_title, { examples }, true)
+#define context(group_title, examples) new uva::cspec::test_group(group_title, { examples }),
 #define it(test_title, test_body) new uva::cspec::test(test_title, test_body),
+
+#ifdef UVA_DATABASE_AVAILABLE
+    #include <database.hpp>
+#endif
 
 namespace uva
 {
     namespace cspec
     {
+        extern std::filesystem::path temp_folder;
         class test_group;
 
         class test_not_passed : public std::runtime_error
@@ -46,7 +55,10 @@ namespace uva
         public:
             const T* m_expected = nullptr;
         };
-
+        class exist
+        {
+        
+        };
         template<typename T>
         class expect
         {
@@ -60,30 +72,113 @@ namespace uva
             const T* m_expected = nullptr;
             bool m_expect_result = true;
         public:
-            const expect<T>& to() const
+            const expect<T>& to_match() const
             {
                 expect<T>* this_non_const = (expect<T>*)this;
                 this_non_const->m_expect_result = true;
                 return *this;
             }
-            const expect<T>& to_not() const
+            const expect<T>& to_not_match() const
             {
                 expect<T>* this_non_const = (expect<T>*)this;
-                this_non_const->m_expect_result = true;
+                this_non_const->m_expect_result = false;
                 return *this;
             }
             template<typename OtherT>
-            friend const expect<T>& operator<<(const expect<T>& expect, const eq<OtherT>& matcher)
+            friend const expect<T>& operator<<(const expect<T>& expected, const eq<OtherT>& matcher)
             {
-                bool match = *expect.m_expected == *matcher.m_expected;
-                bool passed = match ? expect.m_expect_result : !expect.m_expect_result;
+                const T& expect = *expected.m_expected;
+                const OtherT& match = *matcher.m_expected;
 
-                if(!passed) {
-                    throw test_not_passed(std::format("Expected {}\ngot      {}", *expect.m_expected, *matcher.m_expected));
+                bool is_match;
+
+                const constexpr bool expect_is_container = uva::string::is_container<T>::value;
+                const constexpr bool match_is_container = uva::string::is_container<OtherT>::value;
+                const constexpr bool expect_is_string = std::is_same<std::string, T>::value;
+                const constexpr bool match_is_string = std::is_same<std::string, OtherT>::value;
+                const constexpr bool is_container = expect_is_container && match_is_container && !expect_is_string && !match_is_string;
+                
+                if constexpr (is_container)
+                {
+                    if(expect.size() != match.size())
+                    {
+                        is_match = false;
+                    } else 
+                    {
+                        is_match = true;
+
+                        for(size_t index = 0; index < expect.size(); ++index)
+                        {
+                            if(expect[index] != match[index])
+                            {
+                                is_match = false;
+                                break;
+                            }
+                        }
+                    }
+
+                } else {
+                    is_match = expect == match;
                 }
 
-                return expect;
+                bool passed = is_match ? expected.m_expect_result : !expected.m_expect_result;
+
+                if(!passed) {
+                    if constexpr (is_container)
+                    {
+                        std::string expected_str = uva::string::join(*matcher.m_expected, ',');
+                        std::string got_str = uva::string::join(*expected.m_expected, ',');
+
+                        throw test_not_passed(std::format("Expected {{ {} }}\ngot      {{ {} }}", expected_str, got_str));
+
+                    } else 
+                    {
+                       throw test_not_passed(std::format("Expected {}\ngot      {}", *matcher.m_expected, *expected.m_expected));
+                    }
+                }
+
+                return expected;
             }
+
+            friend const expect<T>& operator<<(const expect<T>& expected, const exist& matcher)
+            {
+                if constexpr(std::is_same<T, std::filesystem::path>::value)
+                {
+                    bool is_match = std::filesystem::exists(*expected.m_expected);
+                    bool passed = is_match ? expected.m_expect_result : !expected.m_expect_result;
+
+                    if(!passed) {
+                        if( expected.m_expect_result ) {
+                            throw test_not_passed(std::format("Expected file or directory to exists, but not exists at {}", expected.m_expected->string()));
+                        } else {
+                            throw test_not_passed(std::format("Expected file or directory to not exists, but exists at {}", expected.m_expected->string()));
+                        }
+                    }
+                }
+                #ifdef UVA_DATABASE_AVAILABLE
+                    else if constexpr (std::is_same<T, uva::database::active_record_relation>::value)
+                    {
+                        size_t count = expected.m_expected->count();
+                        bool is_match = (bool)count;
+                        bool passed = is_match ? expected.m_expect_result : !expected.m_expect_result;
+
+                        if(!passed) {
+                            if( expected.m_expect_result ) {
+                                throw test_not_passed(std::format("Expected \"COUNT(*) {}\" or directory to be > 0, but it was {}", expected.m_expected->to_sql(), count));
+                            } else {
+                                throw test_not_passed(std::format("Expected \"COUNT(*) {}\" or directory to be 0, but it was {}", expected.m_expected->to_sql(), count));
+                            }
+                        }
+                    }
+                    else
+                    {
+                        //#error "matcher exist called, but there is no away to know if the expected object exists.";
+                    }
+                #endif
+
+                return expected;
+            }
+
         };
 
         class core
@@ -93,29 +188,68 @@ namespace uva
         public:
             static void run_tests();
         };
-
-        class test
+        class test;
+        class test_base
+        {
+        public:
+            std::vector<const test_base*> tests;
+            virtual void do_test(std::function<void(const test*)> _body) const = 0;
+            bool is_group = false;
+            bool is_root = false;
+            bool is_before_all = false;
+            bool is_before_each = false;
+        };
+        class test : public test_base
         {
         public:
             std::string m_name;
             std::function<void()> m_body;
         public:
             test() = default;
-            test(const std::string& name, const std::function<void()> body)
-                : m_name(name), m_body(body)
-            {
-
-            }
+            test(const std::string& name, const std::function<void()> body);
+            virtual void do_test(std::function<void(const test*)> _body) const override;
         };
-        class test_group
+        struct before_each : public test_base
+        {
+            std::function<void(const test*)> body;
+
+            before_each(std::function<void(const test*)> _body)
+                : test_base(), body(_body)
+            {
+                is_before_each = true;
+            }
+
+            virtual void do_test(std::function<void(const test*)> _body) const override {};
+        };
+        struct before_all : public test_base
+        {
+            std::function<void()> body;
+
+            before_all(std::function<void()> _body)
+                : test_base(), body(_body)
+            {
+                is_before_all = true;
+            }
+
+            virtual void do_test(std::function<void(const test*)> _body) const override {};
+        };
+        class test_group : public test_base
         {
         public:
             std::string m_name;
-            std::vector<test*> m_tests;
+            const before_all* m_beforeAll = nullptr;
+            const before_each* m_beforeEach = nullptr;
         public:
-            test_group(const std::string& name, const std::vector<test*>& tests);
+            test_group(const std::string& name, const std::vector<test_base*>& tests, bool is_root = false);
+            virtual void do_test(std::function<void(const test*)> _body) const override;
         };
     };
 };
 
 using namespace uva::cspec;
+
+#define to to_match() <<
+#define to_not to_not_match() <<
+#define exist exist()
+#define before_all_tests(body) new uva::cspec::before_all(body),
+#define before_each_test(body) new uva::cspec::before_each(body),
--- src/cspec.cpp
+++ src/cspec.cpp
@@ -1,8 +1,13 @@
 #include <sstream>
 
-#include <cspec.hpp>
 #include <console.hpp>
 #include <diagnostics.hpp>
+
+#include <cspec.hpp>
+
+//EXTERN VARIABLES
+
+std::filesystem::path uva::cspec::temp_folder;
 
 //CORE BEGIN
 
@@ -20,20 +25,39 @@ void uva::cspec::core::run_tests()
     size_t examples = 0;
     size_t failures = 0;
 
+    if(std::filesystem::exists(uva::cspec::temp_folder)) {
+        //delete the temp directory
+        std::filesystem::remove_all(uva::cspec::temp_folder);
+    }
+
+    //create the directory if it not exists
+    if(!std::filesystem::create_directories(uva::cspec::temp_folder)) {
+        std::cout << uva::console::color(uva::console::color_code::red) << std::format("Error: failed to create directory: {}", uva::cspec::temp_folder.string()) << std::endl;
+        return;
+    }
+
     auto elapsed = uva::diagnostics::measure_function([&]() {
         for(const auto& test_group : groups)
         {
             std::cout << test_group->m_name << std::endl;
 
-            for(const auto& test : test_group->m_tests)
+            test_group->do_test([&](const uva::cspec::test* test)
             {
                 examples++;
 
                 try {
+                    //disable cout to not print garbage while printing tests output
+                    std::cout.setstate(std::ios_base::failbit);
+                    std::cerr.setstate(std::ios_base::failbit);
                     test->m_body();
+                    std::cout.clear();
+                    std::cerr.clear();
+
                     std::cout << "\t" << uva::console::color(uva::console::color_code::green) << test->m_name << std::endl;
                 } catch(uva::cspec::test_not_passed& e)
                 {
+                    std::cout.clear();
+                    std::cerr.clear();
                     failures++;
                     std::cout << "\t" << uva::console::color(uva::console::color_code::red) << test->m_name << std::endl;
 
@@ -41,12 +65,14 @@ void uva::cspec::core::run_tests()
                 }
                 catch(std::exception& e)
                 {
+                    std::cout.clear();
+                    std::cerr.clear();
                     failures++;
                     std::cout << "\t" << uva::console::color(uva::console::color_code::red) << test->m_name << std::endl;
 
                     failed_messages.push_back(std::format("{} {}\nTest thrown following exception:\n{}", test_group->m_name, test->m_name, e.what()));
                 }
-            }
+            });
 
             std::cout << std::endl;
         }
@@ -59,16 +85,68 @@ void uva::cspec::core::run_tests()
 
     std::cout << std::endl;
     std::cout << failures << " failures / " << examples << " examples." << std::endl;
-    std::cout << "Tests took " << elapsed.count() << "s" << std::endl;
+    std::cout << "Tests took " << std::chrono::duration_cast<std::chrono::seconds>(elapsed).count() << "s" << std::endl;
 }
 
 //CORE END
 
+//TEST BEGIN
+
+//TEST END
+
+uva::cspec::test::test(const std::string& name, const std::function<void()> body)
+    : m_name(name), m_body(body)
+{
+    tests.push_back(this);
+}
+
+void uva::cspec::test::do_test(std::function<void(const uva::cspec::test*)> _body) const
+{
+   _body(this);
+}
+
 //TEST GROUP BEGIN
 
-uva::cspec::test_group::test_group(const std::string& name, const std::vector<uva::cspec::test*>& tests) 
-    : m_name(name), m_tests(tests)
+uva::cspec::test_group::test_group(const std::string& name, const std::vector<uva::cspec::test_base*>& _tests, bool _is_root) 
+    : m_name(name)
 {
-    std::vector<uva::cspec::test_group*>& groups = uva::cspec::core::get_groups();
-    groups.push_back(this);
+    if(_is_root) {
+        std::vector<uva::cspec::test_group*>& groups = uva::cspec::core::get_groups();
+        groups.push_back(this);
+    }
+
+    std::vector<uva::cspec::test_base*> tests_removed_befores = uva::string::select(_tests, [this](uva::cspec::test_base* test){
+        if(test->is_before_all) {
+            m_beforeAll = (uva::cspec::before_all*)test;
+        }
+        if(test->is_before_each) {
+            m_beforeEach = (uva::cspec::before_each*)test;
+        }
+        return !test->is_before_all && !test->is_before_each;
+    });
+
+    tests.insert(tests.begin(), tests_removed_befores.begin(), tests_removed_befores.end());
+
+    is_group = true;
+    is_root = _is_root;
+}
+
+void uva::cspec::test_group::do_test(std::function<void(const uva::cspec::test*)> _body) const
+{
+    if(m_beforeAll) {
+        m_beforeAll->body();
+    }
+    for(const uva::cspec::test_base* test : tests)
+    {
+        if(is_group && !is_root) {
+            std::cout << "\t" << m_name << std::endl;
+            std::cout << "\t";
+
+            if(m_beforeEach) {
+                m_beforeEach->body((uva::cspec::test*)test);
+            }
+        }
+
+        test->do_test(_body);
+    }
 }
