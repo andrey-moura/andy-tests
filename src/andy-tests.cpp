7ee6f065f083a901b8eceaa60658ac0d5cfa79ba
Andrey Moura <andreymds88@gmail.com>
2025-03-30 10:37:12 -0300

    fix: Fix generate XML not escaping special characters

--- src/andy-tests.cpp
+++ src/andy-tests.cpp
@@ -12,6 +12,25 @@ std::vector<andy::tests::test_result> andy::tests::result_list;
 bool andy::tests::has_the_first_specification = false;
 std::vector<std::string_view> andy::tests::current_describe;
 
+void write_safe_str(std::ostream& out, std::string_view str)
+{
+    for(const char& c : str) {
+        if(c == '<') {
+            out << "&lt;";
+        } else if(c == '>') {
+            out << "&gt;";
+        } else if(c == '&') {
+            out << "&amp;";
+        } else if(c == '\'') {
+            out << "&apos;";
+        } else if(c == '\"') {
+            out << "&quot;";
+        } else {
+            out << c;
+        }
+    }
+}
+
 int andy::tests::run()
 {
     auto start = std::chrono::high_resolution_clock::now();
@@ -28,7 +47,9 @@ int andy::tests::run()
 
     // append to file
     std::ofstream file("andy_tests.xml", std::ios::app);
-    file << "\t<testsuite name=\"" << of.m_description << "\" tests=\"" << tests::result_list.size() << "\" failures=\"0\" time=\"" << duration.count() / 1000.0 << "\">" << std::endl;
+    file << "\t<testsuite name=\"";
+    write_safe_str(file, of.m_description);
+    file << "\" tests=\"" << tests::result_list.size() << "\" failures=\"0\" time=\"" << duration.count() / 1000.0 << "\">" << std::endl;
 
     std::cout << std::endl;
     int passed = 0;
@@ -37,16 +58,20 @@ int andy::tests::run()
     for(const auto& result : tests::result_list) {
         file << "\t\t<testcase name=\"";
         if(result.describes.size()) {
-            file << result.describes << " ";
+            write_safe_str(file, result.describes);
+            file << " ";
         }
-        file << result.description << "\" time=\"" << result.duration / 1000.0 << "\">" << std::endl;
+        write_safe_str(file, result.description);
+        file << "\" time=\"" << result.duration / 1000.0 << "\">" << std::endl;
         if(result.passed) {
             ++passed;
         } else {
             ++failed;
             std::cout << std::endl;
 
-            file << "\t\t<failure message=\"" << result.error_message << "\" />" << std::endl;
+            file << "\t\t<failure message=\"";
+            write_safe_str(file, result.error_message);
+            file << "\" />" << std::endl;
 
             uva::console::log_error("{} {}", result.describes, result.description);
             uva::console::log_error("{}", result.error_message);
